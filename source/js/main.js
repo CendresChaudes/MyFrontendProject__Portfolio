90d0d46d3c4c16df56ce3c76acdfdc308a2356c8
CendresChaudes <romqaaa1337@gmail.com>
2023-04-19 17:17:49 +0300

    init: инициализация проекта

---
+++ source/js/main.js
@@ -0,0 +1,9 @@
+import { testWebP } from './modules/utils';
+
+testWebP((support) => {
+  if (support) {
+    document.body.classList.add('webp');
+    return;
+  }
+  document.body.classList.add('no-webp');
+});
