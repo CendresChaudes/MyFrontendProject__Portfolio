2d99adfb602d95f3c4bd4e9bce67e2b0496235c5
CendresChaudes <romqaaa1337@gmail.com>
2023-08-15 10:18:05 +0300

    refactor: заменяет нативный слайдер на swiper
    * Пополняет содержимое слайдера

--- source/js/main.js
+++ source/js/main.js
@@ -1,16 +1,10 @@
 import { testWebP } from './modules/utils';
 import { initMobileMenu } from './modules/mobile-menu';
 import { initScroll } from './modules/scroll';
-import { initSLider } from './modules/slider';
+import { initSlider } from './modules/slider';
 
-testWebP((support) => {
-  if (support) {
-    document.body.classList.add('webp');
-    return;
-  }
-  document.body.classList.add('no-webp');
-});
+testWebP((support) => support ? document.body.classList.add('webp') : document.body.classList.add('no-webp'));
 
 initMobileMenu();
 initScroll();
-initSLider();
+initSlider();
--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,100 +1,29 @@
-import { projects } from './data';
-import { debounce } from './utils';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
-const FIRST_SLIDE_ID = 0;
-const NEXT_SLIDE_DELAY = 175;
-
-const sliderItems = document.querySelectorAll('.slider__item');
-const paginationItems = document.querySelectorAll('.slider__pagination-item');
-const prevSlideButton = document.querySelector('.slider__button--prev');
-const nextSlideButton = document.querySelector('.slider__button--next');
-const slideTitle = document.querySelector('.slider__project-name');
-const viewLink = document.querySelector('.slider__view-link');
-const githubLink = document.querySelector('.slider__github-link');
-
-let currentSlideId = 0;
-
-const removeSlideClasses = () => sliderItems.forEach((item) => item.classList.remove('slider__item--current', 'slider__item--prev', 'slider__item--next'));
-
-const removeCurrentPaginationClass = () => paginationItems.forEach((item) => item.classList.remove('slider__pagination-item--current'));
-
-const getSlidesId = (id) => {
-  currentSlideId = id;
-  let prevSlideId = id - 1;
-  let nextSlideId = id + 1;
-
-  if (nextSlideId === sliderItems.length) {
-    nextSlideId = FIRST_SLIDE_ID;
-  }
-
-  if (prevSlideId === -1) {
-    prevSlideId = sliderItems.length - 1;
-  }
-
-  return { prevSlideId, currentSlideId, nextSlideId };
-};
-
-// eslint-disable-next-line no-shadow
-const changeSlideClass = ({ prevSlideId, currentSlideId, nextSlideId }) => {
-  removeSlideClasses();
-  removeCurrentPaginationClass();
-
-  sliderItems[currentSlideId].classList.add('slider__item--current');
-  sliderItems[prevSlideId].classList.add('slider__item--prev');
-  sliderItems[nextSlideId].classList.add('slider__item--next');
-
-  paginationItems[currentSlideId].classList.add('slider__pagination-item--current');
-};
-
-const fillCurrentSlideInfo = (id) => {
-  slideTitle.textContent = projects[id].title;
-  viewLink.href = projects[id].view;
-  githubLink.href = projects[id].github;
-};
-
-const goToSlide = (id) => {
-  changeSlideClass(getSlidesId(id));
-  fillCurrentSlideInfo(id);
-};
-
-const debouncedGoToSlide = debounce((id) => goToSlide(id), NEXT_SLIDE_DELAY);
-
-const onPrevSlideButtonClick = () => currentSlideId > FIRST_SLIDE_ID ? debouncedGoToSlide(currentSlideId - 1) : debouncedGoToSlide(sliderItems.length - 1);
-
-const onNextSlideButtonClick = () => currentSlideId < (sliderItems.length - 1) ? debouncedGoToSlide(currentSlideId + 1) : debouncedGoToSlide(FIRST_SLIDE_ID);
-
-const defaultSlideInfo = () => {
-  slideTitle.textContent = projects[FIRST_SLIDE_ID].title;
-  viewLink.href = projects[FIRST_SLIDE_ID].view;
-  githubLink.href = projects[FIRST_SLIDE_ID].github;
-};
-
-const setSliderHeight = () => {
-  const slideHeight = getComputedStyle(document.querySelector('.slider__item--current')).height;
-  document.querySelector('.slider__wrapper').style.height = slideHeight;
-};
-
-const onPaginationClick = (index) => {
-  currentSlideId = index;
-  goToSlide(currentSlideId);
-};
-
-const initPaginationListeners = () => {
-  paginationItems.forEach((item, index) => {
-    item
-      .querySelector('button')
-      .addEventListener('click', () => onPaginationClick(index));
+const initSlider = () => {
+  const swiper = new Swiper('.slider', {
+    loop: true,
+    grabCursor: true,
+    effect: 'coverflow',
+    centeredSlides: true,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    breakpoints: {
+      1: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 'auto',
+      },
+    },
+    slideClass: 'slider__item',
   });
 };
 
-const initSLider = () => {
-  defaultSlideInfo();
-  initPaginationListeners();
-
-  prevSlideButton.addEventListener('click', onPrevSlideButtonClick);
-  nextSlideButton.addEventListener('click', onNextSlideButtonClick);
-  window.addEventListener('load', setSliderHeight);
-  window.addEventListener('resize', setSliderHeight);
-};
-
-export { initSLider };
+export { initSlider };
